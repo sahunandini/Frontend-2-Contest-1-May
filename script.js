97cf9f1515eb70a14d339ecb66a703b20970648b
Nandini Sahu <117564136+sahunandini@users.noreply.github.com>
2023-05-13 21:27:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,40 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  const students = arr.filter((student) => student.marks > 50);
+  const studentNames = students.map((student) => student.name);
+  console.log(studentNames);
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((student) => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter((student) => student.marks >= 50);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArr = [
+    { id: 4, name: "alice", age: "18", marks: 90 },
+    { id: 5, name: "bob", age: "19", marks: 75 },
+    { id: 6, name: "charlie", age: "20", marks: 60 },
+  ];
+  arr = arr.concat(newArr);
+  console.log(arr);
 }
